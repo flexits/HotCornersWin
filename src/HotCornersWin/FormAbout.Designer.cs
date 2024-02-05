f8860a5c403226c8833f44e5e0eb79b93f55cd1f
Alexander Korostelin <4d.41.49.4c@gmail.com>
2024-02-05 16:29:38 +0500

    update version

--- src/HotCornersWin/FormAbout.Designer.cs
+++ src/HotCornersWin/FormAbout.Designer.cs
@@ -60,7 +60,7 @@
             labelVer.Name = "labelVer";
             labelVer.Size = new Size(59, 25);
             labelVer.TabIndex = 1;
-            labelVer.Text = "v0.8.3";
+            labelVer.Text = "v0.8.4";
             // 
             // labelDescription
             // 
